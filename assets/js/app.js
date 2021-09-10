a54bebc2dbe6c9d577a12bfc12933096f199cc1a
reneklio <90481082+reneklio@users.noreply.github.com>
2021-09-11 01:02:04 +0300

    MOGO

---
+++ assets/js/app.js
@@ -0,0 +1,70 @@
+$(function() {
+    
+    var header = $("#header"),
+        introH = $("#intro").innerHeight(),
+        scrollofset = $(window).scrollTop();
+    
+    
+    /*Fixed Header*/
+    checkScroll(scrollofset);
+    
+    $(window).on("scroll", function() {
+        scrollofset = $(this).scrollTop();
+        
+        checkScroll(scrollofset);        
+    });
+    
+    function checkScroll(scrollofset){         
+        if( scrollofset >= introH ){
+            header.addClass("fixed")
+        } else {
+            header.removeClass("fixed")
+        }
+    }
+    
+    
+    
+    /*Smooth scroll*/
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+        
+        var $this = $(this),
+            blockId = $this.data("scroll"),
+            blockOffset = $(blockId).offset().top;
+        
+        $("#nav a").removeClass("active");
+        $this.addClass("active");
+        
+        $("html, body").animate({
+            scrollTop: blockOffset
+        }, 500)
+    })
+     
+    
+    /*Menu nav toggle*/
+    
+    $("#nav_toggle").on("click",function(event){
+        event.preventDefault();
+        
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+    });
+    
+    
+    /*Collapse*/
+    
+    $("[data-collapse]").on("click", function(event) {
+        event.preventDefault();
+        
+        var $this = $(this),
+            blockId = $this.data("collapse");
+        
+        $this.toggleClass("active");
+        
+    });
+    
+    
+        
+    
+    
+});   
